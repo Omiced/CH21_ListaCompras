614c288b8c4b39b7acd177be081709b8bd95f391
Josue Cano <73917299+Omiced@users.noreply.github.com>
2022-12-15 23:01:06 +0000

    Agregada persistencia local de datos

--- js/main.js
+++ js/main.js
@@ -7,23 +7,23 @@ const totalProductosEl = document.getElementById("productos-total");
 const totalPrecioEl = document.getElementById("precio-total");
 const resumenEl = document.getElementById("resumen");
 const alertEl = document.getElementById("alertado");
+let productosList = [];
 let id = 0;
 let totalProductos = 0;
 let totalPrecio = 0;
-console.log(formProducts);
 
 const generarPrecio = function () {
   const precio = Math.random() * 100;
   return precio.toFixed(2);
 };
 
-const renderProduct = function (nombre, cantidad, precio) {
+const renderProduct = function (nombre, cantidad, precio, numProduct) {
   const markupProduct = `
 <tr>
-  <th scope="row">${id}</th>
+  <th scope="row">${numProduct}</th>
   <td>${nombre}</td>
   <td>${cantidad}</td>
-  <td>${precio}</td>
+  <td>$ ${precio}</td>
 </tr>
 `;
   productsContainerEl.insertAdjacentHTML("beforeend", markupProduct);
@@ -47,6 +47,17 @@ const validarProducto = function (producto) {
   }
 };
 
+const setLocalRow = function (nombre, cantidad, precio, contador) {
+  const row = {
+    id: contador,
+    nombre: nombre,
+    cantidad: cantidad,
+    precio: precio,
+  };
+  productosList.push(row);
+  localStorage.setItem("productosList", JSON.stringify(productosList));
+};
+
 formProducts.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!productName.value) return;
@@ -59,7 +70,45 @@ formProducts.addEventListener("submit", (e) => {
   id++;
   totalProductos += cantidad;
   totalPrecio += precio * cantidad;
-  renderProduct(nombre, cantidad, precio);
-  changeValues(totalProductos, totalPrecio);
+  localStorage.setItem("productos", id);
+  localStorage.setItem("productosTotal", totalProductos);
+  localStorage.setItem("precioTotal", totalPrecio);
+  renderProduct(nombre, cantidad, precio, id);
+  changeValues(totalProductos, totalPrecio.toFixed(2));
+  setLocalRow(nombre, cantidad, precio, id);
   resumenEl.textContent = id;
+  productName.focus();
+  productName.value = "";
+  productQuantity.value = "";
+});
+
+productName.addEventListener("blur", (e) => {
+  e.preventDefault();
+  e.target.value = productName.value.trim();
+});
+
+window.addEventListener("load", (e) => {
+  const tmpCont = localStorage.getItem("productos");
+  const tmpTotalp = +localStorage.getItem("productosTotal");
+  const tmpTotalpre = parseFloat(localStorage.getItem("precioTotal"));
+  productosList = JSON.parse(localStorage.getItem("productosList"));
+  if (productosList != []) {
+    productosList.forEach((product) =>
+      renderProduct(
+        product.nombre,
+        product.cantidad,
+        product.precio,
+        product.id
+      )
+    );
+  }
+  if (tmpCont != null) {
+    id = tmpCont;
+    resumenEl.textContent = id;
+  }
+  if (tmpTotalp != null && tmpTotalpre != null) {
+    totalProductos = tmpTotalp;
+    totalPrecio = tmpTotalpre;
+    changeValues(totalProductos, totalPrecio.toFixed(2));
+  }
 });
